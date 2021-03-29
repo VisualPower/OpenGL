2237e9f44fffcf535043143ae03dd4f1f4834452
VisualPower <77320713+VisualPower@users.noreply.github.com>
2021-03-29 16:10:20 +0500

    Add files via upload

---
+++ Replicator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+float window;
+window = 0.0f;
+
+private 
+{
+	m_sRenderingType(++)
+
+		glWindowRender( __PURE_APPDOMAIN_GLOBAL )
+}
+
+
+__BASE_FILE__(!window)
+https://github.com/users/VisualPower/projects/1
+
