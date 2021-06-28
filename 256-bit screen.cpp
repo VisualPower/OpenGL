faf9f16ef471bf58989e5b794234cfcaa56f52de
VisualPower <77320713+VisualPower@users.noreply.github.com>
2021-06-28 23:21:40 +0500

    Add files via upload

---
+++ 256-bit screen.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <OpenGL/GL>
+#include <Vulkan/h>
+#include <DirectX/11>
+
+#include<inline>
+
+using namespace std;
+
+int main()
+{
+
+inline const uint160 operator^( base_uint160& a, const base_uint160& b) { return uint160(a) ^= b; }
+inline const uint160 operator&( base_uint160& a, const base_uint160& b) { return uint160(a) &= b; }
+inline const uint160 operator|( base_uint160& a, const base_uint160& b) { return uint160(a) |= b; }
+inline const uint160 operator+( base_uint160& a, const base_uint160& b) { return uint160(a) += b; }
+inline const uint160 operator-( base_uint160& a, const base_uint160& b) { return uint160(a) -= b; }
+
+static(void.const (output.C X*12(Y*2 = Z-)));
+static(void.const (output.C X*13(Y*3 = Z-+)));
+static(void.const (output.C X*14(Y*4 = Z-+/)));
+static(void.const (output.C X*15(Y*5 = Z-+/=)));
+static(void.const (output.C X*16(Y*6 = Z-+/=^)));
+
+int main(void.renderPipeline(Service))
+{
+
+
+
+	
+}
+
+
+
+
+	return 0;
+}
